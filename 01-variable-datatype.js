cbe0acc4d099df6e45897f8328b3657a3a056c91
Anmol Sharma <165986764+sharma21anmol@users.noreply.github.com>
2024-07-13 07:28:53 +0000

    Day one of small scale projects, we understood concept of variable datatype in js

---
+++ 01-variable-datatype.js
@@ -0,0 +1,24 @@
+var numberOne = 92810
+//console.log(numberOne);
+let stringOne = "Anmol here"
+//console.log(stringOne);
+const booleanOne = true
+//console.log(booleanOne)
+//+++++++++++++++++++++++ commenting every console above to improve code reading experience 
+let number = 33421
+//console.log(typeof number);
+let string = "Small Scale Projects"
+//console.log(typeof string);
+let boolean = 1
+//console.log(typeof boolean)
+//console.log(typeof null)
+let array = [2,3,4,5,6]
+//console.log(typeof array);
+//+++++++++++++ commenting every console above to improve code reading experience
+let firstValue = "Hello"
+let secondValue = firstValue.replace("Hello","Namaste")
+console.log(secondValue)
+console.log(firstValue)
+const ErrorCheck = 10
+console.log(ErrorCheck = 11);
+//going to throw an error and i did it to make reader understand that we cannot chnage const value
